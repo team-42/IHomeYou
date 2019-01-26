95c3362b9749d265cf0418475179d8cd80739140
Skamandros <misc@skamandros.de>
2019-01-26 12:32:51 +0100

    Added minimal example

---
+++ src/main/java/de/weareprophet/ihomeyou/HelloLitiGame.java
@@ -0,0 +1,10 @@
+package de.weareprophet.ihomeyou;
+
+import de.gurkenlabs.litiengine.Game;
+
+public class HelloLitiGame {
+    public static void main(String[] args) {
+        Game.init();
+        Game.start();
+    }
+}
