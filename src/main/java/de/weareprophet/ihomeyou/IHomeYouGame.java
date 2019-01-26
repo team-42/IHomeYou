a73d50fcc2c5a1c1b60f085bf8a46ad6a004cbf3
Skamandros <misc@skamandros.de>
2019-01-26 15:45:55 +0100

    Updated build config

--- src/main/java/de/weareprophet/ihomeyou/IHomeYouGame.java
+++ src/main/java/de/weareprophet/ihomeyou/IHomeYouGame.java
@@ -1,6 +1,9 @@
 package de.weareprophet.ihomeyou;
 
 import org.frice.Game;
+import org.frice.obj.sub.ShapeObject;
+import org.frice.resource.graphics.ColorResource;
+import org.frice.util.shape.FRectangle;
 
 import static org.frice.Initializer.launch;
 
@@ -14,5 +17,13 @@ public class IHomeYouGame extends Game {
     public void onInit() {
         setSize(1366, 720);
         setLocation(0, 0);
+
+        ShapeObject obj1 = new ShapeObject(ColorResource.DARK_GRAY, new FRectangle(50, 50));
+        addObject(obj1);
+    }
+
+    @Override
+    public void onExit() {
+        System.exit(0);
     }
 }
