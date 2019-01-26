90cde1079af5d31e54e054336303a73a939bf807
Merando <merando@gmx.de>
2019-01-26 22:35:34 +0100

    Initial Grid Graph Structure

--- src/main/java/de/weareprophet/ihomeyou/GameGrid.java
+++ src/main/java/de/weareprophet/ihomeyou/GameGrid.java
@@ -5,12 +5,18 @@ import com.google.common.collect.Table;
 import org.frice.obj.sub.ImageObject;
 import org.frice.obj.sub.ShapeObject;
 import org.frice.resource.graphics.ColorResource;
+import org.frice.resource.image.ImageResource;
 import org.frice.util.shape.FRectangle;
+import org.jgrapht.Graph;
+import org.jgrapht.alg.util.Pair;
+import org.jgrapht.graph.DefaultEdge;
+import org.jgrapht.graph.SimpleGraph;
 
 
-class GameGrid {
+public class GameGrid {
     private Table<Integer, Integer, ImageObject> gameGrid;
     private IHomeYouGame ihyg;
+    private Graph<Pair<Integer, Integer>, DefaultEdge> graph;
 
     static final int SIZE = 64;
     static final int BORDERS = 10;
@@ -26,19 +32,46 @@ class GameGrid {
         gameGrid.setX(BORDERS);
         gameGrid.setY(BORDERS);
         ihyg.addObject(gameGrid);
+
+        graph = initNoWallGraph();
     }
 
-    public boolean setObject(int row, int column, ImageObject obj) {
+    private Graph<Pair<Integer, Integer>, DefaultEdge> initNoWallGraph() {
+        Graph<Pair<Integer, Integer>, DefaultEdge> graph = new SimpleGraph<>(DefaultEdge.class);
+
+        // add vertices
+        for(int c = 0; c <= COLS+1; c++) {
+            for (int r = 0; r <= ROWS+1; r++) {
+                graph.addVertex(Pair.of(c, r));
+            }
+        }
+
+        // add edges
+        for(int c = 0; c <= COLS+1; c++) {
+            for (int r = 0; r <= ROWS+1; r++) {
+                graph.addVertex(Pair.of(c, r));
+            }
+        }
+
+        return null;
+    }
+
+    public boolean setObject(int row, int column, ImageResource res) {
         if(!gameGrid.contains(row, column)) {
+            ImageObject obj = new ImageObject(res, 64 * column + 32, 64 * row + 32);
             gameGrid.put(row, column, obj);
             ihyg.addObject(obj);
-
             return true;
         }
         return false;
     }
 
-    public void refresh() {
+    public boolean setWall(int row, int column, WallDirection dir) {
+        return false;
+    }
 
+
+    public enum WallDirection {
+        TOP, BOTTOM, LEFT, RIGHT
     }
 }
